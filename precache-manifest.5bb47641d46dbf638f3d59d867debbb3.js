73c0bc460d97d829bcb987e5dd640cd0ec9751e4
emfaizan59 <chaudhryfaizan59@gmail.com>
2020-06-26 11:37:12 +0500

    Updates

---
+++ precache-manifest.5bb47641d46dbf638f3d59d867debbb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a50b4aed86da6ddbc0ce96e74208e0",
+    "url": "/React-MovieSite/index.html"
+  },
+  {
+    "revision": "2d2a5eda6c1879e1c104",
+    "url": "/React-MovieSite/static/css/main.782e965c.chunk.css"
+  },
+  {
+    "revision": "cba95bb64c1efd0328ba",
+    "url": "/React-MovieSite/static/js/2.c29b5d21.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/React-MovieSite/static/js/2.c29b5d21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d2a5eda6c1879e1c104",
+    "url": "/React-MovieSite/static/js/main.4abf66e7.chunk.js"
+  },
+  {
+    "revision": "b3e7c009b8563e385e63",
+    "url": "/React-MovieSite/static/js/runtime-main.5e21793d.js"
+  }
+]);
